8ca87ad80ff02d4188e55a55183a673cb8261b58
Mark Stahl <mjstahl@gmail.com>
2018-09-05 15:33:05 -0400

    Convert to standardjs

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 'use strict'
 
 const handler = {
-  get: function(target, prop) {
-    const attr = target[prop];
+  get: function (target, prop) {
+    const attr = target[prop]
     if (typeof attr === 'function') {
       return new Proxy(attr.bind(target), {
-        apply: function(target, self, args) {
-          const result = target(...args);
-          return (!!result || result === null) ? result : self;
+        apply: function (target, self, args) {
+          const result = target(...args)
+          return (!!result || result === null) ? result : self
         }
-      });
+      })
     }
-    return target[prop];
+    return target[prop]
   }
-};
+}
 
 module.exports = {
   local: new Proxy(window.localStorage, handler),
   session: new Proxy(window.sessionStorage, handler)
-};
+}
